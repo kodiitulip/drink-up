56ccb1ec131538c93b07d04dc0cadd0494dd97b7
Kodie <kodii.tulip@gmail.com>
2024-05-30 01:14:06 -0300

    ✨Starter Files
    This is the begining
    I will suffer

---
+++ src/main/java/io/github/kodiitulip/drinkup/mixin/TitleScreenMixin.java
@@ -0,0 +1,16 @@
+package io.github.kodiitulip.drinkup.mixin;
+
+import io.github.kodiitulip.drinkup.Drinkup;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+    @Inject(method = "init", at = @At("TAIL"))
+    public void onInit(CallbackInfo ci) {
+        Drinkup.LOGGER.info("This line is printed by an example mod mixin!");
+    }
+}
